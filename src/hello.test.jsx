3dcb64ef179c69b530421c71eee10e0c2e712ea7
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-05-15 15:37:08 -0700

    Test header change after button click

--- src/hello.test.jsx
+++ src/hello.test.jsx
@@ -13,14 +13,25 @@ import { describe, it, expect } from 'vitest';
 
 
 import {render, screen } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 import Hello from "./hello.jsx";
 
 describe("Hello component", () => {
     it("renders Magnificent Monkeys", () => {
           // since screen does not have the container property, we'll destructure render to obtain a container for this test
-        // const {container} = render(<Hello/>)
-        render(<Hello />);
-        expect(screen.getByRole("heading").textContent).toMatch(/Magnificent Monkeys/i);
-    })
-})
+        const {container} = render(<Hello/>)
+        expect(container).toMatchSnapshot();
+    });
+
+
+it("renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
+
+    render(<Hello/>);
+    const button = screen.getByRole("button", {name: "Click me!"});
+
+    await user.click(button);
+    expect(screen.getByRole("heading").textContent).toMatch(/Radical Rhinos/i);
+}
+
+)})
