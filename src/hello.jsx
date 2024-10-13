75e1600ec8b354ddbf816a5d595df2de80c23205
Kbouti <121819340+Kbouti@users.noreply.github.com>
2024-10-12 18:08:19 -0700

    Commit old existing changes

--- src/hello.jsx
+++ src/hello.jsx
@@ -1,19 +1,22 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 
 function Hello() {
+  const [heading, setHeading] = useState("Magnificent Monkeys");
 
-const [heading, setHeading] = useState("Magnificent Monkeys");
-
-function clickHandler() {
-    setHeading("Radical Rhinos")
-}
+  function clickHandler() {
+    setHeading("Radical Rhinos");
+  }
 
   return (
     <>
-    <button type="button" onClick={clickHandler}>Click me! </button>
+      <button type="button" onClick={clickHandler}>
+        Click me!{" "}
+      </button>
       <h1>{heading}</h1>
     </>
   );
 }
 
-export default Hello
+
+
+export default Hello;
